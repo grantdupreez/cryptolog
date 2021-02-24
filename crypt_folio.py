e271a463a2b7dc71e4873dc5da9bf70b019c144f
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-02-24 12:11:28 +0000

    Update crypt_folio.py

--- crypt_folio.py
+++ crypt_folio.py
@@ -28,6 +28,7 @@ if uploaded_file is not None:
     st.write(df)
     
     for row in df.iterrows():
-        df['price'] = get_current_price(df.Symbol)
-
+        price = = get_current_price(df.Symbol)
+        st.write(price)
+        
     st.write(df)
