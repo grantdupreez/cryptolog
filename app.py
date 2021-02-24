ba2f7ae6f3c57c25d244df0793f0324db5875746
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-02-24 09:42:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#Import the libraries 
 from bs4 import BeautifulSoup 
 import requests 
 import time
@@ -8,27 +7,16 @@ st.title("Crypto Value Report")
 
 select_currency = st.selectbox('Currency?', ('bitcoin','BTC-GBP','BTC-USD', 'ETH-GBP','ETH-USD', 'ADA-GBP','ADA-USD'))
 
-#Create a function to get the price of a cryptocurrency
 def get_crypto_price(coin):
-#Get the URL
-    url = "https://www.google.com/search?q="+coin+"+price"
-    
-    #Make a request to the website
+    url = "https://www.google.com/search?q="+coin+"+price"    
     HTML = requests.get(url) 
-  
-    #Parse the HTML
     soup = BeautifulSoup(HTML.text, 'html.parser') 
   
     #Find the current price 
     #text = soup.find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
     text = soup.find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).find("div", attrs={'class':'BNeawe iBp4i AP7Wnd'}).text
-#Return the text 
     return text
 
-#Create a main function to consistently show the price of the cryptocurrency
-def main():
-    crypto = select_currency 
-    price = get_crypto_price(crypto)
-    st.write(crypto+' price: ',price)
-
-main()
+crypto = select_currency 
+price = get_crypto_price(crypto)
+st.write(crypto+' price: ',price)
