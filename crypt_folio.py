113c73e97be21bb8920eb8a0c1d43e120149ba37
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2021-02-24 13:38:42 +0000

    Update crypt_folio.py

--- crypt_folio.py
+++ crypt_folio.py
@@ -27,13 +27,6 @@ if uploaded_file is not None:
 
     warnings.filterwarnings('ignore')
     
-#    for index, row in df.iterrows():
-#        df.index['Price'] = get_current_price(row['Symbol'])
-        
-#        st.write(price)
-#        df['Price'] = price
-#        df['Value'] = df.Holding * float(price)
- 
     for i in df.index:
         df.at[i, "Price"] = get_current_price(df.at[i, 'Symbol'])
         df.at[i, "Value"] = df.at[i, 'Price'] * df.at[i, 'Holding']
@@ -41,4 +34,4 @@ if uploaded_file is not None:
         
     st.write(df)
     
-    st.write('Holding value: ' + crypt_total
+    st.write('Holding value: ' + crypt_total)
